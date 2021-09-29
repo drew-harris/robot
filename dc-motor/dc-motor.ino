f7ab6d96ba07b025f96cb165e4952c2f843623bb
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2021-09-28 21:30:09 -0500

    Add vex motor

--- dc-motor/dc-motor.ino
+++ dc-motor/dc-motor.ino
@@ -1,14 +1,22 @@
+#include <vexMotor.h>
 int motorPin = 3;
  
-void setup() 
-{ 
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
-  while (! Serial);
-  Serial.println("Speed 0 to 255");
-  analogWrite(motorPin, 100);
+vexMotor myVexMotor1;
+void setup() { 
+
+vexMotor.attach(3);
+
 } 
  
- 
+void old() {
+
+}
+
 void loop() { 
+  if (Serial.available()) {
+    int speed = Serial.parseInt();
+      Serial.println(speed);
+      vexMotor.write(speed);
+    delay(200);
+  }
 } 
