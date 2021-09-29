c090d7a5d3ceb8f065619ef387fa3d4048717726
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2021-09-28 22:56:43 -0500

    Add warning message for vex-serial.ino

--- vex-serial/vex-serial.ino
+++ vex-serial/vex-serial.ino
@@ -1,10 +1,21 @@
-#include <Servo.h>    // servo library -- vexMotor relies on the Servo library
+#include <Servo.h>
+
+/*
+=============================================================================
+=============================================================================
+===                            IMPORTANT!!!!!!!!                           ==
+=== AT THE BOTTOM OF SERIAL SETTINGS MAKE SURE "NO LINE ENDING IS SELECTED ==
+=============================================================================
+=============================================================================
+ - Drew
+*/
+
 
 int motorPin = 3;
 Servo vexMotor;
 
 int motorControl (int value) {
-vexMotor.write(map(value,-100,100,1000,2000));
+  vexMotor.write(map(value,-100,100,1000,2000));
 }
 
 void setup() {
