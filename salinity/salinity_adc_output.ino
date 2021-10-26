5e5beff87e5ed5e12c812c59f35678bf71eb71bc
qnamhuynh <qhuynh@smu.edu>
2021-10-26 17:45:59 -0500

    New salinity code (now with serial)

--- salinity/salinity_adc_output.ino
+++ salinity/salinity_adc_output.ino
@@ -1,15 +1,18 @@
 #include <KNWRobot.h>
+#include <conductivity.h>
 
 KNWRobot * robot;
 
 void setup() {
   // robot setup
-  robot = new KNWRobot;
+  robot = new KNWRobot();
 }
 
 void loop() {
   int adc,i;
   double mean;
+
+  adc = 0;
   
   // averaging 5 ADC readings
   for (i = 0; i < 5; i++) {
@@ -17,6 +20,7 @@ void loop() {
   }
   mean = (double)adc / 5.0;
   robot->clearLine(1);
-  robot->printLCD(mean); //printing ACD code to LCD
+  //robot->printLCD(mean); //printing ACD code to LCD
+  Serial.println(mean); //printing to serial
   delay(1000); //wait for 1 second
 }
