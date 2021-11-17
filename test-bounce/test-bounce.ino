1f8562e98625fa14404d6fa56b5ad58283e14e76
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2021-11-17 12:45:48 -0600

    Add bounce test?

--- test-bounce/test-bounce.ino
+++ test-bounce/test-bounce.ino
@@ -2,14 +2,12 @@
 
 Robot* robot;
 void setup() {
-    // put your setup code here, to run once:
-      // servo, trigger, echo, ir, salinity servo
-        robot = new Robot(2, 53, 52, 26, 44);
+  // put your setup code here, to run once:
+  // servo, trigger, echo, ir, salinity servo
+  robot = new Robot(2, 53, 52, 26, 44);
         
 }
 void loop() {
-    // put your main code here, to run repeatedly:
   robot->print(String (robot->measureDistance()));
   delay(1000);
-        
 }
