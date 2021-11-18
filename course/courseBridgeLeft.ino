86aced7a4e23ebee02466602e51440fd593fcb13
Cullen Watson <78247585+cgwatson193@users.noreply.github.com>
2021-11-18 12:37:48 -0600

    goes up ramp and gets incline

--- course/courseBridgeLeft.ino
+++ course/courseBridgeLeft.ino
@@ -6,47 +6,54 @@ void setup() {
   robot = new Robot(2, 53, 51, 26, 44);
   robot->turnNav(0);
 
-  // go to first wall  
+  // First turn  
+  //robot->go(90, 30);
+  robot->go(270, 30);
+  while (robot->measureDistance() < 2000) {
+    
+    delay(25);
+  }
+  robot->stop();
+  delay(1000);
+
+  // go forward a certain distance
   robot->go(0, 30);
-   while (robot->measureDistance() > 3000) {
-    
-    delay(50);
-  }
- 
+  delay(1000);
   robot->stop();
-  delay(2000);
 
-  // find first hole
-  robot->go(270);
-  while (robot->measureDistance() < 3000) {
-    
-    delay(50);
+
+  while(robot->getLetters()[0]!='V'){
+    //robot->go(270,30);
+    robot->go(90,30);
+    delay(100);
+    robot->stop();
+    delay(100);
+
   }
-  robot->stop();
-  delay(2000);
 
+
+  // move left a tad
+  robot->stop();
+  delay(1000);
+  robot->go(90,30);
+  delay(1000);
+  robot->stop();
+
+  // go up half of ramp
+  robot->go(0,30);
+  delay(2000);
   
-  // go to second wall
-  robot->go(0);
-  while (robot->measureDistance() > 3000) {
-    
-    delay(50);
-  }
   robot->stop();
+  delay(2000);
+  // say 15 degrees
+  robot->print(String (robot->getIncline()));
   delay(2000);
 
-  // find second hold
-  robot->go(270);
-  while (robot->measureDistance() < 3000) {
-    
-    delay(50);
-  }
-  robot->stop();
-  delay(2000);
-
-  robot->go(0);
-  delay(2000);
-  robot->stop();
+  // go up rest of ramp
+//  robot->go(0, 30);
+//  delay(1000);
+//  robot->stop();
+  
 }  
 void loop() {
 
